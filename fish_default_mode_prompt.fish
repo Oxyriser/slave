276f10e3af994d146b459c16a161bf462c8c72c9
Theo Danneels <theo.danneels@telecom-paristech.fr>
2018-12-15 17:39:19 +0100

    Change the color of the jobs in the right prompt

--- fish_default_mode_prompt.fish
+++ fish_default_mode_prompt.fish
@@ -1,6 +1,7 @@
 function fish_default_mode_prompt
-  if test "$fish_key_bindings" = "fish_vi_key_bindings"
+  if test "$fish_key_bindings" = "fish_vi_key_bindings" 
     or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+
     set -l normal (set_color normal)
     set -l blue (set_color blue)
     set -l yellow (set_color yellow)
