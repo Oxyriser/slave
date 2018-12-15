d8988ddc973e76d40134bd3857884f2ff6c043ce
Theo Danneels <theo.danneels@telecom-paristech.fr>
2018-12-15 18:54:49 +0100

    Modify the git tracking of the upstream branch

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,41 +1,37 @@
 function fish_prompt
-  set -l status_copy $status
-
   set -l red (set_color red)
   set -l blue (set_color blue)
   set -l green (set_color green)
   set -l yellow (set_color yellow)
   set -l normal (set_color normal)
 
-  if test 0 -eq (id -u "$USER")
-    echo -sn $red"#$normal "
-  else
-    set_color normal
+  if test 0 -eq (id -u $USER)
+    echo -sn $red"#"
   end
+  set_color normal
 
-  if test "$PWD" = ~
+  if test $PWD = ~
     echo -sn "~"
   else
-    echo -sn (basename "$PWD")
+    echo -sn (basename $PWD)
   end
 
   if git_is_repo
     set branch_name (git_branch_name)
     set untracked (git_untracked_files)
 
-    set git_glyph ""
+    set git_glyph " "
     if git_is_dirty
       set git_glyph $git_glyph$yellow"!"$normal
     end
-    if test "$untracked" -gt 0
+    if test $untracked -gt 0
       set git_glyph $git_glyph$blue"*"$normal
     end
     if git_is_staged
       set git_glyph $git_glyph$green"+"$normal
     end
     echo -n " $branch_name$git_glyph"
-    echo -n $red(git_ahead "%" "%" "%%")$normal
+    echo -n $red(git_ahead ">" "<" "≠")$normal
   end
-
   echo " "
 end
