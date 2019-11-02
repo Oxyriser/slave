4bafb72a90817c9e4d2acfa84dacdc9acc2c8334
Theo Danneels <danneels.theo@gmail.com>
2019-11-02 17:40:18 +0100

    Add duration in right_prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,17 +1,17 @@
 function fish_right_prompt
   set -l status_copy $status
-
   if test $status_copy -ne 0
-    set_color $fish_color_error
-    echo -sn "($status_copy)"
+    set_color red
+    echo -sn "($status_copy) "
+    set_color normal
   end
 
-  set_color normal
+  set -l duration $CMD_DURATION
+  if test $duration -gt 500
+    echo -n (echo $duration | humanize_duration)" "
+  end
+
   set_color -d
-
-  for i in (jobs | cut -f1)
-    echo -n " %"$i
-  end
-
+  echo -n "%"(jobs | cut -f1)
   set_color normal
 end
