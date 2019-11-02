348d4bd9eccb343dc382a882d081fb41171defd0
Theo Danneels <danneels.theo@gmail.com>
2019-11-02 17:37:46 +0100

    Refacto

--- fish_default_mode_prompt.fish
+++ fish_default_mode_prompt.fish
@@ -1,7 +1,5 @@
 function fish_default_mode_prompt
-  if test "$fish_key_bindings" = "fish_vi_key_bindings" 
-    or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
-
+  if contains $fish_key_bindings fish_vi_key_bindings fish_hybrid_key_bindings
     set -l normal (set_color normal)
     set -l blue (set_color blue)
     set -l yellow (set_color yellow)
--- fish_prompt.fish
+++ fish_prompt.fish
@@ -7,40 +7,44 @@ function fish_prompt
   set -l yellow (set_color yellow)
   set -l bold (set_color -o white)
   set -l normal (set_color normal)
+  set -l space " "
 
   if test 0 -eq (id -u $USER)
-    echo -sn $red"#"$normal
+    echo -sn $red "#" $normal$space
   end
 
   if set -q VIRTUAL_ENV
-    echo -sn  $yellow "(" (basename $VIRTUAL_ENV) ")" $normal
+    echo -sn  $yellow "("(basename $VIRTUAL_ENV)")" $normal$space
   end
 
-  if test $PWD = ~
-    echo -sn $bold"~"$normal
+  if test $PWD = "~"
+    set path "~"
   else
-    echo -sn $bold(basename $PWD)$normal
+    set path (basename $PWD)
   end
+  echo -sn $bold $path $normal$space
 
   if git_is_repo
-    set branch_name (git_branch_name)
-    set untracked (git_untracked_files)
+    set -l git_glyph ""
 
-    set git_glyph ""
     if git_is_dirty
       set git_glyph $git_glyph$yellow"!"$normal
     end
-    if test $untracked -gt 0
+
+    if test (git_untracked_files) -gt 0
       set git_glyph $git_glyph$blue"*"$normal
     end
+
     if git_is_stashed
       set git_glyph $git_glyph$magenta"^"$normal
     end
+
     if git_is_staged
       set git_glyph $git_glyph$green"+"$normal
     end
-    echo -n " "$branch_name$git_glyph$normal
-    echo -n $red(git_ahead ">" "<" "≠")$normal
+
+    set git_glyph $git_glyph$red(git_ahead ">" "<" "≠")
+
+    echo -n (git_branch_name)$git_glyph$normal$space
   end
-  echo " "
 end
